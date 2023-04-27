5bedf05c7d0cd79a6e69ee139452b2d8e717e5be
Anselem Odimegwu <odimegwu@systemspecs.com.ng>
2023-04-27 09:09:03 +0100

    Feat: client-server architecture setup

---
+++ client/src/__tests__/Checkout.test.ts
@@ -0,0 +1,70 @@
+// // write test cases for Checkout component using jest and react testing library
+
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import Checkout from '../components/Checkout';
+
+// describe('Checkout', () => {
+//   test('renders Checkout component', () => {
+//     render(<Checkout />);
+//     const linkElement = screen.getByText(/Checkout/i);
+//     expect(linkElement).toBeInTheDocument();
+//   });
+// });
+
+// // write test cases for ProductList component using jest and react testing library
+
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import ProductList from '../components/ProductList';
+
+// describe('ProductList', () => {
+//   test('renders ProductList component', () => {
+//     render(<ProductList />);
+//     const linkElement = screen.getByText(/ProductList/i);
+//     expect(linkElement).toBeInTheDocument();
+//   });
+// });
+
+// // write test cases for ProductItem component using jest and react testing library
+
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import ProductItem from '../components/ProductItem';
+
+// describe('ProductItem', () => {
+//   test('renders ProductItem component', () => {
+//     render(<ProductItem />);
+//     const linkElement = screen.getByText(/ProductItem/i);
+//     expect(linkElement).toBeInTheDocument();
+//   });
+// });
+
+// // write test cases for Cart component using jest and react testing library
+
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import Cart from '../components/Cart';
+
+// describe('Cart', () => {
+//   test('renders Cart component', () => {
+//     render(<Cart />);
+//     const linkElement = screen.getByText(/Cart/i);
+//     expect(linkElement).toBeInTheDocument();
+//   });
+// });
+
+// // write test cases for OrderSummary component using jest and react testing library
+
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import OrderSummary from '../components/OrderSummary';
+
+// describe('OrderSummary', () => {
+//   test('renders OrderSummary component', () => {
+//     render(<OrderSummary />);
+//     const linkElement = screen.getByText(/OrderSummary/i);
+//     expect(linkElement).toBeInTheDocument();
+//   });
+// });
+
