85d7ac4de4bc3bbd91cd8d765dc9cffe4b9909bc
EUNICE JACOB <115852649+eunicegigijacob@users.noreply.github.com>
2023-04-27 15:02:42 +0100

    add loading state to conditionally display elements.

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { getProducts } from "./api";
-import LoadingIcon from "./LoadingIcon";
+import React, { useState, useEffect } from 'react';
+import { getProducts } from './api';
+import LoadingIcon from './LoadingIcon';
 
 const Product = ({ product, addProductToCart, removeFromCart }) => {
   return (
@@ -18,7 +18,11 @@ const Product = ({ product, addProductToCart, removeFromCart }) => {
   );
 };
 
-export const ProductList = ({ products, addProductToCart, removeProductFromCart }) => {
+export const ProductList = ({
+  products,
+  addProductToCart,
+  removeProductFromCart,
+}) => {
   return (
     <div>
       <h1>Health Products</h1>
@@ -36,52 +40,72 @@ export const ProductList = ({ products, addProductToCart, removeProductFromCart 
 
 const Checkout = () => {
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsloading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
-      const data = await getProducts();
-      setProducts(data);
+      try {
+        const data = await getProducts();
+        setProducts(data);
+        setIsloading(false);
+      } catch (error) {
+        console.log(error.message);
+      }
     }
     fetchData();
   }, []);
+
   const addProductToCart = (productId) => {};
 
   const removeProductFromCart = (productId) => {};
   return (
     <>
-      <div className="product-grid">
-        <ProductList products={products} addProductToCart={addProductToCart} removeProductFromCart={removeProductFromCart} />
-      </div>
-      <div className="checkout-grid">
-        <h1>Order Summary</h1>
-        <table className="order-summary-table">
-          <thead>
-            <tr>
-              <th>Product</th>
-              <th>Price</th>
-              <th>Quantity</th>
-              <th>Total</th>
-            </tr>
-          </thead>
-          <tbody>
-            {products.map((item) => (
-              <tr key={item.id}>
-                <td>{item.name}</td>
-                <td>${item.price}</td>
-                <td>
-                  <button>-</button>
-                  {item.quantity}
-                  <button>+</button>
-                </td>
-                <td>${item.price}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <p>Subtotal: $0</p>
-        <p>Discount: $0</p>
-        <p>Total: $0</p>
-      </div>
+      {isLoading ? (
+        <>
+          {' '}
+          <LoadingIcon />
+        </>
+      ) : (
+        <>
+          <div className="product-grid">
+            <ProductList
+              products={products}
+              addProductToCart={addProductToCart}
+              removeProductFromCart={removeProductFromCart}
+            />
+          </div>
+          <div className="checkout-grid">
+            <h1>Order Summary</h1>
+            <table className="order-summary-table">
+              <thead>
+                <tr>
+                  <th>Product</th>
+                  <th>Price</th>
+                  <th>Quantity</th>
+                  <th>Total</th>
+                </tr>
+              </thead>
+              <tbody>
+                {products.map((item) => (
+                  <tr key={item.id}>
+                    <td>{item.name}</td>
+                    <td>${item.price}</td>
+                    <td>
+                      <button>-</button>
+                      {item.quantity}
+                      <button>+</button>
+                    </td>
+                    <td>${item.price}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <p>Subtotal: $0</p>
+            <p>Discount: $0</p>
+            <p>Total: $0</p>
+          </div>
+        </>
+      )}
     </>
   );
 };
