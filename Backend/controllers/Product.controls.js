df7df35a3eeb6a8921db420f314345fd19d0e580
EUNICE JACOB <115852649+eunicegigijacob@users.noreply.github.com>
2023-04-28 12:59:24 +0100

    create controller functions for products
    controls for getting all products, add new products,

---
+++ Backend/controllers/Product.controls.js
@@ -0,0 +1,37 @@
+const { productServices } = require('../services/product');
+
+const productControls = {
+  getAllProducts: async (req, res) => {
+    try {
+      const products = await productServices.getProducts();
+
+      res.status(200).json(products);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  addProducts: async (req, res) => {
+    const { id, name, description, price, quantity } = req.body;
+
+    try {
+      const product = await productServices.addProduct({
+        id,
+        name,
+        description,
+        price,
+        quantity,
+      });
+
+      res.status(200).json(product);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  getCart: async (req, res) => {
+    res.send('cart');
+  },
+};
+
+module.exports = { productControls };
