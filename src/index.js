98f46801688ab512625f56009fe5d6c60ee96a71
Letam Bossman Barinua <letambarinua@gmail.com>
2023-04-28 02:57:47 +0100

    Move logo svg file to assets under src folder

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
+import React from "react";
 import App from "./components/App/App";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root")
