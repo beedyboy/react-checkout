27b8d997e1837cc7688796cd794c7e72b245d6b7
Admolgit <zealous890@gmail.com>
2023-05-02 00:35:16 +0100

    Task Done

--- src/LoadingIcon.js
+++ src/LoadingIcon.js
@@ -1,9 +1,10 @@
-import "./LoadingIcon.css";
+import './LoadingIcon.css';
+import { Spin } from 'react-cssfx-loading';
 
 const LoadingIcon = ({ isLoading }) => {
   return (
-    <div className={`loading-icon ${isLoading ? "loading" : ""}`}>
-      <span>Health Shop</span>
+    <div className={`loading-icon ${isLoading ? 'loading' : ''}`}>
+      <Spin color="#8a2b06" width="70px" height="70px" duration="2s" />
     </div>
   );
 };
