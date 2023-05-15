938415ef3e71d2972df8171047d1828be2edd0a2
Admolgit <zealous890@gmail.com>
2023-05-15 12:00:28 +0100

    added features

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,36 +1,44 @@
 import React, { useState, useEffect } from 'react';
-// import { getProducts } from './api';
-import axios from 'axios';
+import { getProducts } from './api';
+// import axios from 'axios';
 import LoadingIcon from './LoadingIcon';
 
-const Product = ({ product, addProductToCart, removeProductFromCart, cartItem }) => {
-
+const Product = ({
+  product,
+  addProductToCart,
+  removeProductFromCart,
+  cartItem,
+}) => {
   const [disableAdd, setDisableAdd] = useState(false);
   const [disableRemove, setDisableRemove] = useState(false);
-  
+
   useEffect(() => {
-    if(cartItem.length > 0 ) {
-      setDisableRemove(false)
-      if( cartItem[0].quantity >= product.quantity) {
-        setDisableAdd(true)
+    if (cartItem.length > 0) {
+      setDisableRemove(false);
+      if (cartItem[0].quantity >= product.quantity) {
+        setDisableAdd(true);
       } else {
-        setDisableAdd(false)
+        setDisableAdd(false);
       }
     } else {
-      setDisableRemove(true)
+      setDisableRemove(true);
     }
   }, [cartItem]);
-  
+
   return (
     <div className="product-card">
       <h2>{product.name}</h2>
       <p>{product.description}</p>
       <p>Price: ${product.price}</p>
       <p className="product-quantity">
-        <button disabled={disableAdd} onClick={addProductToCart}>Add</button>
+        <button disabled={disableAdd} onClick={addProductToCart}>
+          Add
+        </button>
         Quantity:
         <span>{product.quantity}</span>
-        <button disabled={disableRemove} onClick={removeProductFromCart}>Remove</button>
+        <button disabled={disableRemove} onClick={removeProductFromCart}>
+          Remove
+        </button>
       </p>
     </div>
   );
@@ -40,7 +48,7 @@ export const ProductList = ({
   products,
   addProductToCart,
   removeProductFromCart,
-  cart
+  cart,
 }) => {
   return (
     <div>
@@ -53,7 +61,7 @@ export const ProductList = ({
               product={product}
               addProductToCart={() => addProductToCart(product.id)}
               removeProductFromCart={() => removeProductFromCart(product.id)}
-              cartItem={cart.filter(item => item.id === product.id)}
+              cartItem={cart.filter((item) => item.id === product.id)}
             />
           </div>
         ))}
@@ -63,16 +71,15 @@ export const ProductList = ({
 };
 
 export const CartItem = (props) => {
-
   const [disableAdd, setDisableAdd] = useState(false);
-  
+
   useEffect(() => {
-      if( props.quantity >= props.product.quantity) {
-        setDisableAdd(true)
-      } else {
-        setDisableAdd(false)
-      }
-    }, [props.quantity]);
+    if (props.quantity >= props.product.quantity) {
+      setDisableAdd(true);
+    } else {
+      setDisableAdd(false);
+    }
+  }, [props.quantity]);
 
   return (
     <tr key={props.id}>
@@ -81,9 +88,11 @@ export const CartItem = (props) => {
       <td>
         <button onClick={props.onRemove}>-</button>
         {Number(props.quantity)}
-        <button disabled={disableAdd} onClick={props.onAdd}>+</button>
+        <button disabled={disableAdd} onClick={props.onAdd}>
+          +
+        </button>
       </td>
-      <td>{(props.price* props.quantity).toFixed(2)}</td>
+      <td>{(props.price * props.quantity).toFixed(2)}</td>
     </tr>
   );
 };
@@ -100,68 +109,69 @@ const Checkout = () => {
     async function fetchData() {
       setIsLoading(true);
       // Using the provided JSON data
-      // const data = await getProducts();
-      // setProducts(data);
+      const data = await getProducts();
+      setProducts(data);
       /* Products from a mongodb node backend check how to start server in the readme file*/
-      const { data } = await axios.get("http://localhost:4008/api/products")
-      setProducts(data.data);
+      // const { data } = await axios.get("http://localhost:4008/api/products")
+      // setProducts(data.data);
       setIsLoading(false);
     }
     fetchData();
   }, []);
 
   const getTotal = () => {
-    const grossTotal = cartItems.reduce((a, b) => (b.price * b.quantity) + a, 0)
+    const grossTotal = cartItems.reduce((a, b) => b.price * b.quantity + a, 0);
 
-    setSubTotal(grossTotal)
+    setSubTotal(grossTotal);
 
     if (grossTotal > 1000) {
       const discountPercentAmount = grossTotal * 0.1;
       const discountedAmount = grossTotal - discountPercentAmount;
-      setTotalAmount(discountedAmount)
+      setTotalAmount(discountedAmount);
       setDiscount(discountPercentAmount);
     } else {
-      setTotalAmount(grossTotal)
+      setTotalAmount(grossTotal);
     }
-  }
+  };
 
   useEffect(() => {
-    getTotal()
-  }, [cartItems])
+    getTotal();
+  }, [cartItems]);
 
   const addProductToCart = (productId) => {
-
     let product = products.filter((item) => item.id === productId);
 
-    const itemIndex = cartItems.findIndex(item => item.id === productId)
+    const itemIndex = cartItems.findIndex((item) => item.id === productId);
 
-    const updatedItems = [...cartItems]
-    if(itemIndex >= 0) {
-      product = updatedItems[itemIndex]
-      updatedItems[itemIndex] = {...product, quantity: product.quantity + 1}
+    const updatedItems = [...cartItems];
+    if (itemIndex >= 0) {
+      product = updatedItems[itemIndex];
+      updatedItems[itemIndex] = { ...product, quantity: product.quantity + 1 };
     } else {
-      updatedItems.push({...product[0], quantity: 1})
+      updatedItems.push({ ...product[0], quantity: 1 });
     }
 
     setCartItems(updatedItems);
   };
 
   const removeProductFromCart = (productId) => {
-
-    const itemIndex = cartItems.findIndex(item => item.id === productId)
+    const itemIndex = cartItems.findIndex((item) => item.id === productId);
 
     let updatedItems = [...cartItems];
 
-    if(itemIndex >= 0) {
-      const product = updatedItems[itemIndex]
-      if(product.quantity === 1) {
-        updatedItems = cartItems.filter((item) => item.id !== productId)
+    if (itemIndex >= 0) {
+      const product = updatedItems[itemIndex];
+      if (product.quantity === 1) {
+        updatedItems = cartItems.filter((item) => item.id !== productId);
       } else {
-        updatedItems[itemIndex] = {...product, quantity: product.quantity - 1}
+        updatedItems[itemIndex] = {
+          ...product,
+          quantity: product.quantity - 1,
+        };
       }
     }
 
-    setCartItems(updatedItems)
+    setCartItems(updatedItems);
   };
 
   if (isLoading) {
@@ -206,7 +216,7 @@ const Checkout = () => {
                 quantity={product.quantity}
                 onRemove={removeProductFromCart.bind(null, product.id)}
                 onAdd={addProductToCart.bind(null, product.id)}
-                product={products.filter(item => item.id === product.id)[0]}
+                product={products.filter((item) => item.id === product.id)[0]}
               />
             ))}
           </tbody>
